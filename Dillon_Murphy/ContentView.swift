a2449a602b97f92e164715e8ba364e1f9483b4bc
Dillon Murphy <dillon@concealedcarry.com>
2021-02-10 18:10:26 -0700

    Initial Commit

---
+++ Dillon_Murphy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dillon_Murphy
+//
+//  Created by Dillon Murphy on 2/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dillon_Murphy/Dillon_MurphyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dillon_MurphyApp.swift
+//  Dillon_Murphy
+//
+//  Created by Dillon Murphy on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Dillon_MurphyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
